51e239275bbc6c114287258a9e0aa823fa3f3386
Ardel Tioco Jeff Lauron <ardeltiocojefflauron@gmail.com>
2024-03-25 18:42:04 +0800

    Added Remove Post Feature

---
+++ WheelDeal/Lab_Version/includes/delete_post.php
@@ -0,0 +1,19 @@
+<?php
+    include '../connect.php';
+    $post_id_to_delete = $_GET["post_id"];
+    $username = $_GET["username"];
+    $acctid = $_GET["acctid"];
+    
+    $delete_query = 
+    "UPDATE post 
+    SET is_active = 0
+    WHERE post_id = ?";
+
+    $update_query = $conn->prepare($delete_query);
+    $update_query->bind_param("i",$post_id_to_delete);
+
+    if($update_query->execute()){
+        header("Location: ../main_page.php?username=$username&acctid=$acctid");
+    }
+    echo('I will delete post with post id:'.$post_id_to_delete);
+?>
--- WheelDeal/Lab_Version/includes/display.php
+++ WheelDeal/Lab_Version/includes/display.php
@@ -25,13 +25,53 @@
             echo'<div id="error" style="width: 100%; text-align: center; color:red;">There are no uploads </div>';
         }
 
+        ?>
+        <?php
         while($row = mysqli_fetch_assoc($result)){
+            echo
+            '
+            <div class="delete-post bg-body rounded-4 mb-3 shadow p-3 " id="post-'.$row['post_id'].'">
+                <div class="confirm-message">
+                    Are you sure you want to remove post?
+                </div>
+                <div class="choice-container container d-flex justify-content-center">
+                    <div class="btn-no btn btn-outline-primary m-2" id="no-'.$row['post_id'].'">
+                        No
+                    </div>
+                    <a href="/includes/delete_post.php?post_id='.$row['post_id'].'&username='.$username.'&acctid='.$acctid.'" class="btn-yes btn btn-danger m-2">Yes</a>
+                </div>
+            </div>
+            ';
+            echo
+                ('
+                    <script>
+                    document.getElementById("no-'.$row['post_id'].'").addEventListener("click", function(){
+                            document.getElementById("post-'.$row['post_id'].'").style.display="none";
+                    })
+                    document.getElementById("yes-'.$row['post_id'].'").addEventListener("click", function(){
+                        alert("YOU ARE DELETING POST '.$row['post_id'].'");
+                    })
+                    </script>
+                ');
             echo'<div class="card-div mb-3 shadow bg-body p-3 rounded-4">';
             if($isAdmin || $acctid == $row['author_id']){
                 echo '
                 <div class="delete-btn-container">
-                    <button type="button" class="btn-close" aria-label="Close"></button>
+                    <button type="button" id="delete-post-'.$row['post_id'].'" class="position-absolute btn-close" aria-label="Close"></button>
                 </div>';
+                echo
+                ('
+                    <script>
+                    document.getElementById("delete-post-'.$row['post_id'].'").addEventListener("click", function(){
+                        if(document.getElementById("post-'.$row['post_id'].'").style.display=="block"){
+                            document.getElementById("post-'.$row['post_id'].'").style.display="none";
+                        }
+                        else{
+                            document.getElementById("post-'.$row['post_id'].'").style.display="block";
+                        }
+                    })
+                    </script>
+                ');
             }
             echo'    <div class="row name-div p-3">
                     '.$row["username"].'
